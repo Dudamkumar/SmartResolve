e63b2f0912a339c4849c469512b6299706a376b0
Dudam-Kumar <161219033+Dudamkumar@users.noreply.github.com>
2026-09-23 13:16:53 +0530

    Update API base URL to smartresolve-3

--- smartresolve-frontend/src/services/api.js
+++ smartresolve-frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    "https://smartresolve-2.onrender.com/",
+    "https://smartresolve-3.onrender.com/",
 
   headers: {
     "Content-Type": "application/json",
