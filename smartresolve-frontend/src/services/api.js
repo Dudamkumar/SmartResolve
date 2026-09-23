feb736aa4ab4b5fa0a73bac1560fc90a9e52de78
Dudam-Kumar <161219033+Dudamkumar@users.noreply.github.com>
2026-09-23 12:54:51 +0530

    Change API base URL to production endpoint

--- smartresolve-frontend/src/services/api.js
+++ smartresolve-frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    "http://localhost:8080",
+    "https://smartresolve-2.onrender.com/",
 
   headers: {
     "Content-Type": "application/json",
